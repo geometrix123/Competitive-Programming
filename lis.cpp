e35f20b130b9dc5f46c3bad530da768e34ec15e6
Yeboi <76149417+geometrix123@users.noreply.github.com>
2021-04-21 07:37:44 +0530

    Add files via upload

---
+++ lis.cpp
@@ -0,0 +1,19 @@
+ll lis(vector<ll> const& a) {
+    ll n = a.size();
+    const ll INF = 1e9;
+    vector<ll> d(n+1, INF);
+    d[0] = -INF;
+
+    for (ll i = 0; i < n; i++) {
+        ll j = upper_bound(d.begin(), d.end(), a[i]) - d.begin();
+        if (d[j-1] < a[i] && a[i] < d[j])
+            d[j] = a[i];
+    }
+
+    ll ans = 0;
+    for (ll i = 0; i <= n; i++) {
+        if (d[i] < INF)
+            ans = i;
+    }
+    return ans;
+}
